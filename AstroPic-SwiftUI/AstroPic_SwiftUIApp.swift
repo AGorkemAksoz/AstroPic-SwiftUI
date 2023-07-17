d2b08b4853f5fa7ad3ef9051333059cc628b4b03
AGorkemAksoz <a.gorkemaksoz@gmail.com>
2023-07-17 10:43:06 +0300

    Initial Commit

---
+++ AstroPic-SwiftUI/AstroPic_SwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  AstroPic_SwiftUIApp.swift
+//  AstroPic-SwiftUI
+//
+//  Created by Gorkem on 17.07.2023.
+//
+
+import SwiftUI
+
+@main
+struct AstroPic_SwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
